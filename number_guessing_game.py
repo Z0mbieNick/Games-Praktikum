fa87c03bcc073d64d1fcccad7691c595d4b14f7c
Z0mbieNick <nicolas.evdokimov@icloud.com>
2024-12-19 20:41:38 +0100

    test _ commit

---
+++ number_guessing_game.py
@@ -0,0 +1,23 @@
+
+# This is the number guessing game
+import random
+
+while True:
+    status = True
+    random_number = random.randint(1, 100)
+    print("\nErrate die Zahl, welche zwischen 1 und 100 liegt: ")
+
+    while status:
+        guess = int(input("Gib eine Zahl ein: "))
+
+        if guess == random_number:
+            print("Glückwunsch! Ihr hast die Zahl erraten.\n")
+            status = False
+        elif guess < random_number:
+            print("Zu niedrig!")
+        elif guess > random_number:
+            print("Zu hoch!")
+        else:
+            print("Ungültige Eingabe")
+
+        
