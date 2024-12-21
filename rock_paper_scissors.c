51009922cc3242b49c145dd097e879e4b5bd866c
Z0mbieNick <nicolas.evdokimov@icloud.com>
2024-12-21 11:49:19 +0100

    Fix computer guess logic in Rock, Paper, Scissors game

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -3,4 +3,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int main(){
+
+    unsigned int seed = time(NULL);
+    srand(seed);
+    int computer_guess_chooser = rand() % 3;
+
+
+    if (computer_guess_chooser == 0){
+        char computer_guess = "rock";
+    } else if (computer_guess_chooser == 1){
+        char computer_guess = "paper";
+    } else{
+        char computer_guess = "scissors";
+    }
+
+    printf("%s", computer_guess);
+
+
+
+}
